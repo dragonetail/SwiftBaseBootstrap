0e178bc85fecec90f0fba796b12491f8d22c52a3
dragonetail <dragonetail@gmail.com>
2018-12-15 23:21:55 +0800

    Release 0\.1\.5

---
+++ pod_publish.sh
@@ -0,0 +1,19 @@
+
+PODSPEC=`ls *.podspec`
+PREVersion="$1"
+TARGETVersion="$2"
+
+if test -z "${PREVersion}" -o -z "${TARGETVersion}"
+then
+	echo "pod_publish <PREVersion> <TARGETVersion>"
+	exit -1
+fi
+
+PREVersion=${PREVersion//\./\\\.}
+TARGETVersion=${TARGETVersion//\./\\\.}
+
+sed -i '' "s/${PREVersion}/${TARGETVersion}/g" ${PODSPEC}
+git add -A && git commit -m "Release ${TARGETVersion}" . && git push
+git tag -f '${TARGETVersion}' && git push -f --tags
+pod trunk push ${PODSPEC}
+
